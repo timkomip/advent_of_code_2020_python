6f5cccf3d5f5d23233a0d0073086039e41c1ce43
Michael Timko <timkomip@gmail.com>
2020-12-22 16:15:05 -0500

    day 3 part 2

--- day03/main.py
+++ day03/main.py
@@ -14,10 +14,15 @@ class Map:
 
 
 class Marker:
-    def __init__(self, x, y, map):
-        self.x = x
-        self.y = y
+    def __init__(self, init_x, init_y, map):
+        self.init_x = init_x
+        self.init_y = init_y
         self.map = map
+        self.reset()
+    
+    def reset(self):
+        self.x = self.init_x
+        self.y = self.init_y
         self.trees_encountered = set()
 
         self.record_location()
@@ -26,7 +31,7 @@ class Marker:
         self.x += x
         self.y = min(self.map.height - 1, self.y + y)
         self.record_location()
-
+    
     def record_location(self):
         if self.map.is_tree(self.x, self.y):
             self.trees_encountered.add((self.x, self.y))
@@ -39,16 +44,32 @@ class Marker:
     def pos(self):
         return (self.x, self.y)
 
+def count_trees_to_corner(map, slope):
+    marker = Marker(0,0, map)
+    while True:
+        if marker.at_bottom:
+            break
+        marker.move(*slope)
+    return len(marker.trees_encountered)
+
 lines = []
 with open('input.txt') as f:
     lines = f.readlines()
 
 forest_map = Map(lines)
-marker = Marker(0,0, forest_map)
 
-while True:
-    if marker.at_bottom:
-        break
-    marker.move(3, 1)
+slopes = [
+    (1, 1),
+    (3, 1),
+    (5, 1),
+    (7, 1),
+    (1, 2),
+]
 
-print(f'Trees encountered: {len(marker.trees_encountered)}')
+result = 1
+for slope in slopes:
+    count = count_trees_to_corner(forest_map, slope)
+    print(f'Trees encountered for slope {slope}: {count}')
+    result *= count
+
+print(f"Product of tree counts: {result}")
